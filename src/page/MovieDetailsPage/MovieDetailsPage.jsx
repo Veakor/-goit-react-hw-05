d72016ca05b45c4e1c41520e618eba75f4dbd6e8
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-08 11:47:33 +0200

    add

--- src/page/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/page/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { useParams, useLocation, Link } from 'react-router-dom';
-import MovieReviews from '';
+import MovieReviews from '../';
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
