5511563a32100da1c7d2059e8fe928eb6a667be7
Veakor <135655247+Veakor@users.noreply.github.com>
2024-04-11 16:31:10 +0200

    add

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,35 +1,14 @@
-import { useEffect, useState } from "react";
-import { getTrendMovies } from "../../servic/API";
-import { Link, useLocation } from "react-router-dom";
 
-const MoviesList = () => {
-  const [topMovies, setTopMovies] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const location = useLocation();
+//import { getTrendMovies } from "../../servic/API";
+import { Link} from "react-router-dom";
 
-  useEffect(() => {
-    async function fetchTopMovies() {
-      try {
-        const data = await getTrendMovies();
-        setTopMovies(data);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-    
-    fetchTopMovies();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error}</div>;
+const MoviesList = ({movies}) => {
+  
 
   return (
     <div>
       <ul>
-      {topMovies.map(movie => (
+      {movies.map(movie => (
         <li key={movie.id}>
           <Link to={{
             pathname: `/movies/${movie.id}`,
